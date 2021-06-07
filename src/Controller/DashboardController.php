f9b2b0bcdebd2b5ed2f0739670a5cc8798736ba4
abdfa <65665408+abdfa@users.noreply.github.com>
2021-06-07 09:48:38 +0000

    Update DashboardController.php

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -73,8 +73,7 @@ class DashboardController extends BaseController
                     'icon'	=> 'usb.png',
                     'text'	=> "Transférer les risques projets",
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER'),
-                    'path'	=> $this->generateUrl('dashboard')
-                    //'path'	=> $this->generateUrl('les_risques_a_transferer')
+                    'path'	=> $this->generateUrl('les_risques_a_transferer')
                 )
 
             )
@@ -90,36 +89,31 @@ class DashboardController extends BaseController
                     'text'	=> "Risque valide",
                     'width' => '50%',
                     'roles' =>array('ROLE_PORTEUR'),
-                    'path'	=> $this->generateUrl('dashboard')
-                    //'path'	=> $this->generateUrl('choix_carto',array('carto' =>$default_carto_by_profil, 'link'=>'les_risques'))
+                    'path'	=> $this->generateUrl('choix_carto',array('carto' =>$default_carto_by_profil, 'link'=>'les_risques'))
                 ), array(
                     'icon'	=> 'satellite.png',
                     'text'	=> "Contrôle",
                     'width' => '40%',
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_AUDITEUR', 'ROLE_RESPONSABLE', 'ROLE_CHEFPROJET', 'ROLE_SUPERVISEUR', 'ROLE_PORTEUR'),
-                    //'path'	=> $this->generateUrl('les_controles')
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $this->generateUrl('les_controles')
                 ), array(
                     'icon'	=> 'checkmark.png',
                     'text'	=> "Risque rejeté",
                     'width' => '50%',
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_AUDITEUR', 'ROLE_RESPONSABLE', 'ROLE_CHEFPROJET'),
-                    'path'	=> $this->generateUrl('dashboard')
-                    //'path'	=> $this->generateUrl('choix_carto',array('carto' =>$default_carto_by_profil, 'link'=>'les_risques_rejetes'))
+                    'path'	=> $this->generateUrl('choix_carto',array('carto' =>$default_carto_by_profil, 'link'=>'les_risques_rejetes'))
                 ),array(
                     'icon'	=> 'hammer.png',
                     'text'	=> "Plan d'action",
                     'width' => '40%',
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_AUDITEUR', 'ROLE_RESPONSABLE', 'ROLE_CHEFPROJET', 'ROLE_SUPERVISEUR', 'ROLE_PORTEUR'),
-                    //'path'	=> $this->generateUrl('les_planactions')
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $this->generateUrl('les_planactions')
                 ), array(
                     'icon'	=> 'stats_lines.png',
                     'text'	=> "Matrice",
                     'width' => '40%',
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_AUDITEUR', 'ROLE_RESPONSABLE', 'ROLE_CHEFPROJET'),
-                    //'path'	=> $this->generateUrl('choix_carto_kpi', array('carto'=>1, 'type'=>4, 'link'=>'la_restitution'))
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $this->generateUrl('choix_carto_kpi', array('carto'=>1, 'type'=>4, 'link'=>'la_restitution'))
                 )
             )
         );
@@ -141,14 +135,12 @@ class DashboardController extends BaseController
                     'icon'	=> 'firewall.png',
                     'text'	=> "Tableau de bord",
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_RESPONSABLE', 'ROLE_AUDITEUR'),
-                    // 'path'	=> $typeSh? $this->generateUrl('choix_type',array('link'=>'documents','year'=>date('Y'), 'type'=>$typeSh->getId())):'#'
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $typeSh? $this->generateUrl('choix_type',array('link'=>'documents','year'=>date('Y'), 'type'=>$typeSh->getId())):'#'
                 ), array(
                     'icon'	=> 'abacus.png',
                     'text'	=> "Veille",
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_RESPONSABLE', 'ROLE_AUDITEUR'),
-                    'path'	=> $this->generateUrl('dashboard')
-                    //'path'	=> $typeVeille?$this->generateUrl('choix_type',array('link'=>'documents','year'=>date('Y'), 'type'=>$typeVeille->getId())):'#'
+                    'path'	=> $typeVeille?$this->generateUrl('choix_type',array('link'=>'documents','year'=>date('Y'), 'type'=>$typeVeille->getId())):'#'
                 ), array(
                     'icon'	=> 'calculator.png',
                     'text'	=> "Formation",
@@ -167,15 +159,13 @@ class DashboardController extends BaseController
             'header'	=> "Administration et Exploitation",
             'roles'		=> array('ROLE_ADMIN', 'ROLE_RISKMANAGER', 'ROLE_RESPONSABLE'),
             //'rows'		=> array(),
-            //'path'		=> ($this->getUser()->hasRole('ROLE_ADMIN')|| $this->getUser()->hasRole('ROLE_RISKMANAGER'))?$this->generateUrl('les_processus'):'#',
-            'path'	=> $this->generateUrl('dashboard'),
+            'path'		=> ($this->getUser()->hasRole('ROLE_ADMIN')|| $this->getUser()->hasRole('ROLE_RISKMANAGER'))?$this->generateUrl('les_processus'):'#',
             'rows'		=> array(
                 array(
                     'icon'	=> 'briefcase.png',
                     'text'	=> "Suivi projet",
                     'roles' => array('ROLE_ADMIN', 'ROLE_RESPONSABLE'),
-                    // 'path'	=> $this->generateUrl('suivi_projet')
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $this->generateUrl('suivi_projet')
                 ),
                 array(
                     'icon'	=> 'gear.png',
@@ -187,8 +177,7 @@ class DashboardController extends BaseController
                     'icon'	=> 'add.png',
                     'text'	=> "Chargements",
                     'roles' => array('ROLE_ADMIN', 'ROLE_RISKMANAGER'),
-                    // 'path'	=> $this->generateUrl('les_chargements')
-                    'path'	=> $this->generateUrl('dashboard')
+                    'path'	=> $this->generateUrl('les_chargements')
                 )/*,
     					array(
     							'icon'	=> 'cutter.png',
